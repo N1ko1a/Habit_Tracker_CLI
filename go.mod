d4ac06f9c3fc111013af618231f77eb6580fe204
N1ko1a <ivanovicn98@gmail.com>
2024-04-13 18:48:01 +0200

    Dodao sam mogucnost dodavanja navika

--- go.mod
+++ go.mod
@@ -4,12 +4,23 @@ go 1.22.2
 
 require (
 	github.com/Delta456/box-cli-maker/v2 v2.3.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gookit/color v1.5.2 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/sys v0.1.0 // indirect
-	golang.org/x/term v0.0.0-20221017184919-83659145692c // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.15.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
 	"time"
 )
+
+type Habit struct {
+	Name  string   `json:"name"`
+	Boxes []string `json:"boxes"`
+}
+
+// kreiramo instancu da bi se povezali sa bazom i imali interakcije
+var mongoClient *mongo.Client
+
+const uri = "mongodb://localhost:27017"
 
 func daysInMonth(year int, month time.Month) int {
 	return time.Date(year, month+1, 0, 0, 0, 0, 0, time.UTC).Day()
@@ -20,7 +35,39 @@ func drawBox(width int) string {
 	return box
 }
 
+func addTask(habit Habit) error {
+	ctx := context.TODO()                            //vraca prazan context koji sluzi kao placeholder
+	Database := mongoClient.Database("HabitTracker") //Ovo je baza, koristimo instancu inzad koju smo kreirali
+	Collection := Database.Collection("Habits")      //Ovo je colekcija, koristimo umesto instance ime baze
+
+	_, err := Collection.InsertOne(ctx, bson.D{
+		{Key: "name", Value: habit.Name},
+		{Key: "boxes", Value: habit.Boxes},
+	})
+
+	return err
+
+}
+
 func main() {
+
+	// Povezivanje sa bazom podataka
+	ctx := context.TODO()
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	var err error
+	mongoClient, err = mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		fmt.Printf("error connecting to MongoDB: %s\n", err.Error())
+		return
+	}
+	defer func() {
+		if err := mongoClient.Disconnect(ctx); err != nil {
+			fmt.Printf("error disconnecting from MongoDB: %s\n", err.Error())
+		}
+	}()
+
 	t := time.Now()
 	year := t.Year()
 	month := t.Month()
@@ -38,4 +85,18 @@ func main() {
 		fmt.Printf("%s ", boxes[i])
 	}
 	fmt.Println()
+	args := os.Args
+	if len(args) >= 2 {
+		habitName := args[2]
+
+		// Create a Habit object
+		habit := Habit{
+			Name:  habitName,
+			Boxes: boxes, // Initialize array with 30 empty strings
+		}
+
+		// Call addTask with the created Habit object
+		addTask(habit)
+	}
+
 }
